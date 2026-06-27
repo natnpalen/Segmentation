a3ef9f10324bd4ac3d6603ce759ee89584aa8235
natnpalen <nathanielapalencia@gmail.com>
2026-06-26 22:56:31 -0700

    Update run_156L1.m

--- Scaphoid_Project/src/run_156L1.m
+++ Scaphoid_Project/src/run_156L1.m
@@ -4,7 +4,7 @@
 %
 % Paths are set to the 156L-1 scan and mechanical specimen STL files.
 % Edit below if your files are in a different location.
-
+clear all; close all;
 dicomFolder = 'C:\Users\natha\OneDrive\Documents\Nathaniel\For Nick\New Bone Scans\156L-1\DICOMOBJ';
 stlFolder   = 'C:\Users\natha\OneDrive\Documents\Nathaniel\For Nick\Mechancial Specimens';
 
